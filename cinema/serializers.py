16c02ce601214b6009a7c30cfe06206cfd3d03c9
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-10 21:40:57 +0200

    "created serializers, views and urls"

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,29 @@
-# write serializers here
+from rest_framework import serializers
+from .models import Actor, CinemaHall, Genre, Movie, MovieSession, Order, Ticket
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ["id", "first_name", "last_name"]
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["id", "name"]
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "description", "duration", "genres", "actors"]
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ["id", "name", "rows", "seats_in_row"]
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = ["id", "show_time", "movie", "cinema_hall"]
+
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,8 @@
-# write urls here
+from django.contrib import admin
+from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
+
+admin.site.register(CinemaHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Movie)
+admin.site.register(MovieSession)
